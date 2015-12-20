af18f877e34932c182369f75b0a6bec1bb88d9b7
persineti <persineti1@gmail.com>
2015-12-20 15:26:35 -0500

    updated from GitHub
    directly did the changes in Git Hub

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,7 +11,8 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.Write("HEloo wokfedo");
-            Console.WriteLine("Will you impact the changes which i did VS")
+            Console.WriteLine("Will you impact the changes which i did VS");
+            Console.WriteLine("Will this impact the chnges which i did GITHb");
 
 
         }
